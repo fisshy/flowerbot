b12a11d6247929b05eb2050aa61fa95a7a28b819
Joachim Karlsson <Jockkee_@hotmail.com>
2017-04-15 12:34:27 +0200

    removed some code

--- client/app.js
+++ client/app.js
@@ -18,7 +18,7 @@ $.getJSON( "/db.json", function( db ) {
                     date.getMinutes()),
             y: c.celsius,
             fanOn: c.fanOn,
-            segmentColor: c.fanOn ? 'rgba(244,67,54, 1)' : 'rgba(244,67,54,0.5)'
+            color: c.fanOn ? 'blue' : 'red'
         }
     });
 
@@ -73,46 +73,17 @@ $.getJSON( "/db.json", function( db ) {
 
        tooltip: {
             formatter: function() {
-                console.log("this.point", this.point);
                 return 'Temperatur <b>' + this.point.y + '℃</b><br/>' +
                     "Fläkt på: " + (this.point.fanOn ? "Ja" : "Nej");
             }
         },
 
        series: [{
-           type: 'coloredarea',
+           type: 'area',
            name: 'Temperatur',
            turboThreshold: 0,
            data: data
        }]
    });
-
-   var series = chart.series[0];
-
-    $.each(series.data, function(i, point) {
-        point.update({
-            marker: {
-                fillColor: {
-                    linearGradient: {
-                        x1: 0,
-                        y1: 0,
-                        x2: 0,
-                        y2: 1
-                    },
-                    stops: [
-                        [0, Highcharts.getOptions().colors[1]],
-                        [1, Highcharts.Color(Highcharts.getOptions().colors[1]).setOpacity(0).get('rgba')]
-                    ]
-                },
-                states: {
-                	hover: {
-                    	fillColor: 'red',
-                    	lineColor: 'red'
-                	}
-            	}
-            }
-        });
-    });
-    series.redraw();
 
 });
